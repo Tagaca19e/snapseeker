38fc683ec16278907a72b227b0fa474332875f89
tillo93 <123791681+tillo93@users.noreply.github.com>
2023-02-28 20:04:32 -0800

    Hey, I think I have a good start to the barcode scanner. I am not getting any errors when it compiles but it won't display on the dashboard. I can't figure out what I am doing wrong to start looking at the barcode feature. Can someone take a look and let me know.

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,11 +1,12 @@
 import { getSession } from 'next-auth/react';
 import { signOut } from 'next-auth/react';
+import { BarcodeReader } from './api/auth/Barcode-scan';
 
 export default function dashboard({ email }) {
   return (
     <div>
       <div>
-        <h1>Hello {email}</h1>
+              <h1>Hello {email}</h1>
         <a
           onClick={() => signOut()}
           className="hover:bg-primary-dark focus:ring-primary-light focus:border-primary-darkest active:bg-primary-darkest flex w-max cursor-pointer justify-center rounded-md border border-transparent bg-cyan-800 py-2 px-4 text-sm font-medium text-white transition duration-150 ease-in-out focus:outline-none focus:ring-4"
@@ -35,4 +36,15 @@ export async function getServerSideProps(context) {
       email: session.user.email,
     },
   };
+}
+
+export function Scan() {
+    console.log('Scan being called');
+
+    return (
+        <div>
+            <h1>Barcode Reader</h1>
+            <BarcodeReader />
+        </div>
+    );
 }
