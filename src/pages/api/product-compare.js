83f314904a7c082fa53135170e65afc969b460ae
tillo93 <123791681+tillo93@users.noreply.github.com>
2023-03-29 19:05:35 -0700

    product comp
    compare prices button shows if there is multiple places carrying item, and a window pops up showing the diff companies and diff prices when you click the button

---
+++ src/pages/api/product-compare.js
@@ -0,0 +1,30 @@
+import SerpApi from 'google-search-results-nodejs';
+
+export default async function getResults(req, res) {
+  const productId = req.query?.productId;
+  // if (req.method === 'GET') {
+  //   res.status(400).json({ message: 'Invalid request method' });
+  //   return;
+  // }
+
+  const search = new SerpApi.GoogleSearch(process.env.SERP_API_KEY);
+  try {
+    search.json(
+      {
+        engine: 'google_product',
+        google_domain: 'google.com',
+        product_id: productId,
+        offers: '1',
+        hl: 'en',
+        gl: 'us',
+
+      },
+      (data) => {
+        /* @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/201 */
+        res.status(201).json({ data });
+      }
+    );
+  } catch (error) {
+    res.status(400).json({ message: 'Something went wrong with search.' });
+  }
+}
